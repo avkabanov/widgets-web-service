d0b1853731a2ed23c91a9325324ea584fdd05d59
avkabanov <usrobotics1>
2019-12-02 03:28:01 +0300

    added tests

--- src/main/resources/ddl.sql
+++
@@ -1,7 +0,0 @@
-create table WIDGET (
-  ID bigint primary key not null generated always as identity,
-  ACCOUNT_TYPE varchar(1) not null,
-  EMAIL varchar(50) not null,
-  AMOUNT double default 0,
-  OVERDRAFT double default 0
-);
--- src/test/java/com/kabanov/widgets/interseptor/rate_limit/BucketTest.java
+++ src/test/java/com/kabanov/widgets/interseptor/rate_limit/BucketTest.java
@@ -4,18 +4,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.kabanov.widgets.interseptor.rate_limit.data.Bucket;
+import com.kabanov.widgets.interseptor.rate_limit.data.ConsumptionProbe;
 
 /**
  * @author Kabanov Alexey
  */
 public class BucketTest {
-   // TODO fix tests
+
     @Test
     public void shouldReturnMaxTokensOnRefill() {
         Bucket bucket = new Bucket(10);
+        bucket.refill(System.currentTimeMillis());
         Assert.assertTrue(bucket.tryConsume(10).isConsumed());
         Assert.assertFalse(bucket.tryConsume(1).isConsumed());
-        
+
         bucket.refill(System.currentTimeMillis());
         Assert.assertTrue(bucket.tryConsume(10).isConsumed());
         Assert.assertFalse(bucket.tryConsume(1).isConsumed());
@@ -24,8 +26,39 @@ public class BucketTest {
     @Test
     public void shouldNotBeAbleToConsumeMoreTokensThanExist() {
         Bucket bucket = new Bucket(10);
+        bucket.refill(System.currentTimeMillis());
+
         Assert.assertTrue(bucket.tryConsume(10).isConsumed());
         Assert.assertFalse(bucket.tryConsume(1).isConsumed());
     }
 
+    @Test
+    public void shouldReturnValidRemainingTokensOnConsume() {
+        Bucket bucket = new Bucket(10);
+        bucket.refill(System.currentTimeMillis());
+
+        ConsumptionProbe result = bucket.tryConsume(1);
+        Assert.assertEquals(9, result.getRemainingTokens());
+        Assert.assertTrue(result.isConsumed());
+    }
+
+    @Test
+    public void shouldBeAbleToConsumeAllTokens() {
+        Bucket bucket = new Bucket(10);
+        bucket.refill(System.currentTimeMillis());
+
+        ConsumptionProbe result = bucket.tryConsume(10);
+        Assert.assertEquals(0, result.getRemainingTokens());
+        Assert.assertTrue(result.isConsumed());
+    }
+
+    @Test
+    public void shouldNotBeAbleToConsumeMoreThanExistingTokens() {
+        Bucket bucket = new Bucket(10);
+        bucket.refill(System.currentTimeMillis());
+
+        ConsumptionProbe result = bucket.tryConsume(11);
+        Assert.assertFalse(result.isConsumed());
+        Assert.assertEquals(10, result.getRemainingTokens());
+    }
 }
